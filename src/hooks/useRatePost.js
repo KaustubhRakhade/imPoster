fc9385ee31c9ec0b554133aec006329810c03ac4
KaustubhRakhade <kaustubhrakhade@gmail.com>
2023-07-26 22:07:52 +0530

    first commit

--- src/hooks/useRatePost.js
+++ src/hooks/useRatePost.js
@@ -3,16 +3,15 @@ export default function useRatePost(setData) {
         setData((oldData) => {
 
             const toRate = oldData.filter((p) => p.timeStamp === timeStamp)[0]
-            console.log("rating")
             let newData = [...oldData]
 
             if (rating === 1) {
-                newData[newData.indexOf(toRate)].likeCount += newData[newData.indexOf(toRate)].liked ? 1 : 0
+                newData[newData.indexOf(toRate)].likeCount += 1
                 newData[newData.indexOf(toRate)].liked = true;
                 newData[newData.indexOf(toRate)].dislikeCount -= newData[newData.indexOf(toRate)].disliked ? 1 : 0
                 newData[newData.indexOf(toRate)].disliked = false
             } else if (rating === -1) {
-                newData[newData.indexOf(toRate)].dislikeCount += newData[newData.indexOf(toRate)].disliked ? 1 : 0
+                newData[newData.indexOf(toRate)].dislikeCount += 1
                 newData[newData.indexOf(toRate)].disliked = true;
                 newData[newData.indexOf(toRate)].likeCount -= newData[newData.indexOf(toRate)].liked ? 1 : 0
                 newData[newData.indexOf(toRate)].liked = false
@@ -23,8 +22,6 @@ export default function useRatePost(setData) {
                 newData[newData.indexOf(toRate)].disliked = false
             }
 
-            console.log("rating", newData[newData.indexOf(toRate)].likeCount)
-            
             return newData;
         })
     }
